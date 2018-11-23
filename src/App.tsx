cb581c69aff577a33d8d0c551040730606c50a40
ponkys <diego.mosquera.soto@gmail.com>
2018-11-23 15:45:39 +0100

    css changes

--- src/App.tsx
+++ src/App.tsx
@@ -8,11 +8,13 @@ import { Calculator } from './calculator/Calculator';
 class App extends Component {
     render() {
         return (
-            <div className="of-util of-shared text-center">
-                <div className="of-content-layout padding-horizontal-gutter margin-horizontal-auto">
-                    <p>Budget</p>
+            <div className="main-layout of-util of-shared text-center">
+                <div className="nav-bar">Billullo</div>
+                <div className="of-content-layout display-flex flex-column calculator-content padding-horizontal-gutter margin-horizontal-auto">
                     <Calculator />
                 </div>
+                <div className="add-area display-flex">Add</div>
+                {/* <div onClick={() => this.onAdd(expense)}>Add</div> */}
             </div>
         );
     }
